54e969ed88bcc9964db590d6ce8a03db266e02dd
Maicon Borba <maiconbsconceicao@gmail.com>
2025-08-11 13:16:13 -0300

    preparing to build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), react()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
